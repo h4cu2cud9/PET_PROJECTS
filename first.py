e5155e304b8e8aae72cb09b8d73001faa3e8c5cd
h4cu2cud9 <155180606+h4cu2cud9@users.noreply.github.com>
2026-07-18 14:45:39 +0300

    Main development stage

--- first.py
+++ first.py
@@ -2,24 +2,36 @@ import requests
 
 url_d = "https://api.nbrb.by/exrates/rates?periodicity=0"
 url_m = "https://api.nbrb.by/exrates/rates?periodicity=1"
-resp_d = requests.get(url_d)
-resp_m = requests.get(url_m)
-data_d = resp_d.json()
-data_m = resp_m.json()
+result = []
 
-for i in data_d:
-    print(i['Cur_Name'], i['Cur_OfficialRate'])
+class Parser:
+    def __init__(self, url_d, url_m, result):
+        self.url_d = url_d
+        self.url_m = url_m
+        self.result = result
 
-print('\n')
+    def resp(self):
+        try:
+            combined = []
+            resp_d = requests.get(self.url_d)
+            resp_d.raise_for_status()
+            resp_m = requests.get(self.url_m)
+            resp_m.raise_for_status()
+            combined = resp_d.json() + resp_m.json()
+            for i in combined:
+                self.result.append(i['Cur_Abbreviation'])
+                self.result.append(i['Cur_Name'])
+                self.result.append(i['Cur_OfficialRate'])
+        except:
+            raise requests.exceptions.RequestException
 
-for j in data_m:
-    print(j.get('Cur_Name'), j.get('Cur_OfficialRate'))
-#parent class
-'Cur_Abbreviation'
-'Cur_QuotName'
-...
+    def file_creator(self):
+        with open('cur_today.csv', 'w', encoding='utf-8') as file:
+            for idx, i in enumerate(self.result, start=1):
+                file.write(str(i))
+                file.write('\n' if idx % 3 == 0 else ' ')
 
 
-#child class
-
-
+p = Parser(url_d, url_m, result)
+print(p.resp())
+print(p.file_creator())
